5522532504307f184344343548b1bd9832b74b84
Zineb2a <zineb.alaoui663@gmail.com>
2026-03-15 10:56:33 -0400

    feat: update greeting messages in Home component to use translation keys

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -412,9 +412,9 @@ export default function Home() {
 
   const getGreeting = () => {
     const hour = new Date().getHours();
-    if (hour < 12) return "Good morning";
-    if (hour < 18) return "Good afternoon";
-    return "Good evening";
+    if (hour < 12) return t("home.goodMorning");
+    if (hour < 18) return t("home.goodAfternoon");
+    return t("home.goodEvening");
   };
 
   const handleContributeToGoal = async () => {
